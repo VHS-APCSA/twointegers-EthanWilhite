b2f8e5592f7ddec71783593167ba0ffcca9af8c6
bob <bob@APCSA>
2018-09-05 14:52:12 -0700

    learning math

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -5,13 +5,44 @@ public class TwoIntegers
 
 	public TwoIntegers()
 	{
-	num1 = 0;
-	num2 = 0;
+		num1 = 0;
+		num2 = 0;
+	}
+
+	public TwoIntegers(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	public int getNum1()
+	{
+		return num1;
+	}
+	public void setNum1(int num1)
+	{
+		this.num1 = num1;
+	}
+	public int getNum2()
+	{
+		return num2;
+	}
+	public void setNum2(int num2)
+	{
+		this.num2 = num2;
+	}
+	public TwoIntegers addition(TwoIntegers twoint)
+	{
+		int value1 = this.value1 + twoint.getNum1();
+		int value2 = this.value2 + twoint.getNum2();
+		TwoIntegers result = new TwoIntegers(value1, value2);
+		return result;
+	}
+
+
+
+
+
+
 }
 
-public TwoIntegers(int num1, int num2)
-{
-this.num1 = num1;
-this.num2 = num2;
-}
-}
+
