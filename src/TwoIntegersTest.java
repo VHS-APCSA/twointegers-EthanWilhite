69dd9163607a4222616af99d1e082698759dedbc
bob <bob@APCSA>
2018-09-11 15:30:41 -0700

    adding the larger() method

--- src/TwoIntegersTest.java
+++ src/TwoIntegersTest.java
@@ -1,9 +1,9 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class TwoIntegersTest 
 {
 	public static void main(String[] args)
-	{
-		TwoIntegers twointegers = new TwoIntegers(6,13);
+	{ 
+		Two Integers twointegers = new TwoIntegers(6,13);
 		int num1 = twointegers.getNum1();
 		int num2 = twointegers.getNum2();
 		String arithmetic = twointegers.displayArithmetic();
