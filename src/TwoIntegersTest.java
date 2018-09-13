9f50eb0102d9dedb53cbaecda50c83532610719d
bob <bob@APCSA>
2018-09-13 10:19:07 -0700

    almost finished with the runner. I have to include two values that go with the Scanner

--- src/TwoIntegersTest.java
+++ src/TwoIntegersTest.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
 public class TwoIntegersTest 
 {
 	public static void main(String[] args)
 	{ 
+		Scanner input = new Scanner(System.in);
 		TwoIntegers twointegers = new TwoIntegers(6,13);
 		int num1 = twointegers.getNum1();
 		int num2 = twointegers.getNum2();
 		String arithmetic = twointegers.displayArithmetic();
 		System.out.print(arithmetic);
+		int larger = twointegers.larger();
+		System.out.println(larger + " is larger than num1 so it was returned as true ");
+		boolean isEven = twointegers.isEven();
+		System.out.println(num2 + " is not even so = " + isEven);
+		boolean isMultiple = twointegers.isMultiple();
+		System.out.println(num2 + " is not a multiple of 6 so = "+ isMultiple );
+		System.out.print(" Enter a number: ");
+		int value = input.nextInt();
+		
+		
 	}
+	
+	
+
+
+
+
+
 }
